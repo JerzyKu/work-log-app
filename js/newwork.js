a15b4a42ba3f75d65dd270aba6199844b9289d64
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2021-12-09 14:15:25 +0100

    some work

--- js/newwork.js
+++ js/newwork.js
@@ -1,14 +1,41 @@
 const settings = {
-    'company-A': ['task-A-A','task-A-B','task-A-C'],
-    'company-B': ['task-B-A','task-B-B','task-B-C'],
-    'company-C': ['task-C-A'],
-    'company-D': []
+    "company-A": ["task-A-A", "task-A-B", "task-A-C"],
+    "company-B": ["task-B-A", "task-B-B", "task-B-C"],
+    "company-C": ["task-C-A"],
+    "company-D": null
 }
+
+// settings = JSON.parse(settings) // shame u stupid 
 
 const company = document.querySelector('#primary')
 const tasks = document.querySelector('#secondary')
 
-Object.keys(settings).forEach( e => {
-    console.log(e);
+const create_option_element = (e) => {
+    new_ele = document.createElement('option')
+    new_ele.value = e
+    new_ele.innerText = e
+    return new_ele
+}
+
+Object.keys(settings).forEach(e => {
+    company.append(create_option_element(e))
+})
+
+settings[company.value].forEach( e => {
+    tasks.append(create_option_element(e))
+})
+
+company.addEventListener('change', () => {
+    key = company.value
+    tasks.innerHTML = ''
+    console.log(settings[key]);
+    if (settings[key] === null) {
+        tasks.append(create_option_element(' - - - '))
+        console.log('---');
+        return
+    }
+    settings[key].forEach(e => {
+        tasks.append(create_option_element(e))
+    })
 })
 
--- js/work.js
+++ js/work.js
@@ -6,49 +6,82 @@ start_button.addEventListener("click", () => {
 
     const company = document.querySelector('#primary').value
     const task = document.querySelector('#secondary').value
-    const description = document.querySelector('#textarea_description').value
+    var description = document.querySelector('#textarea_description').value
+    if (description == '') description = ' - - - '
     const start_date = new Date()
-    const start_sec = start_date.getSeconds()
-    const start_min = start_date.getMinutes()
-    const start_hour = start_date.getHours()
 
     const task_id = start_date.getTime()
 
-    newDiv = document.createElement("div");
-    newDiv.id = 'm' + task_id
+    // Get a reference to the table
+    let tableRef = document.getElementById('done-work-table');
 
-    let newEle = document.createElement("span")
-    newEle.innerText = company
-    newDiv.append(newEle)
+    // Insert a row at the end of the table
+    let newRow = tableRef.insertRow(01);
+    newRow.id = `m${task_id}`; // idk why need to add semicoma 
 
-    newEle = document.createElement("span")
-    newEle.innerText = task
-    newDiv.append(newEle)
 
-    newEle = document.createElement("span")
-    newEle.innerText = description != '' ? description : "Empty"
-    newDiv.append(newEle)
-
-    newEle = document.createElement("span")
-    newEle.innerText = start_date.getDate()
-    newDiv.append(newEle)
-
-    newDiv.append(document.createElement("br"))
-
-    newEle = document.createElement("span")
-    newEle.innerText = `${start_hour < 10 ? '0' : ''}${start_hour}:${start_min < 10 ? '0' : ''}${start_min}:${start_sec < 10 ? '0' : ''}${start_sec}`
-    newDiv.append(newEle)
-
-    newEle = document.createElement("button")
-    newEle.innerText = 'Stop'
-    newEle.addEventListener('click', () => {
-        document.querySelector('#m' + task_id).remove()
+    [company, task, description, generate_clock_string(start_date), 'test'].forEach((e, index) => {
+        console.log(e, index)
+        newCell = newRow.insertCell(index);
+        newText = document.createTextNode(`${e}`);
+        newCell.appendChild(newText);
     })
-    newDiv.append(newEle)
 
-    newDiv.append(document.createElement("br"))
+    newCell = newRow.insertCell(-1)
+    new_ele = document.createElement('button')
+    new_ele.innerText = 'stop'
+    new_ele.addEventListener('click', () => {
+        tableRef.deleteRow(`m${task_id}`);
+    })
+    
+    newCell.appendChild(new_ele);
 
-    document.querySelector('.active-works').append(newDiv)
+    // newEle = document.createElement("button")
+    // newEle.innerText = 'Stop'
+    // newEle.addEventListener('click', () => {
+    //     alert('TO OD !!!')
+    // })
+    // newDiv.append(newEle)
+
+
+
+
+
+    // newDiv = document.createElement("div");
+    // newDiv.id = 'm' + task_id
+
+    // let newEle = document.createElement("span")
+    // newEle.innerText = company
+    // newDiv.append(newEle)
+
+    // newEle = document.createElement("span")
+    // newEle.innerText = task
+    // newDiv.append(newEle)
+
+    // newEle = document.createElement("span")
+    // newEle.innerText = description != '' ? description : "Empty"
+    // newDiv.append(newEle)
+
+    // newEle = document.createElement("span")
+    // newEle.innerText = start_date.getDate()
+    // newDiv.append(newEle)
+
+    // newDiv.append(document.createElement("br"))
+
+    // newEle = document.createElement("span")
+    // newEle.innerText = `${start_hour < 10 ? '0' : ''}${start_hour}:${start_min < 10 ? '0' : ''}${start_min}:${start_sec < 10 ? '0' : ''}${start_sec}`
+    // newDiv.append(newEle)
+
+    // newEle = document.createElement("button")
+    // newEle.innerText = 'Stop'
+    // newEle.addEventListener('click', () => {
+    //     document.querySelector('#m' + task_id).remove()
+    // })
+    // newDiv.append(newEle)
+
+    // newDiv.append(document.createElement("br"))
+
+    // document.querySelector('.active-works').append(newDiv)
 
 })
 
